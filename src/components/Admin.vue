ceb4344744961c21726e339e54a30b9daf52847c
thisisayushsharma <thisisayushsharma@gmail.com>
2022-05-17 17:09:06 -0500

    Ayush Added Admin

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,38 +1,78 @@
 <template>
 <v-app id="inspire">
+  <v-app-bar color="red" class="flex-grow-0" app dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-title>RESPER</v-app-bar-title>
+    </v-app-bar>
      <v-navigation-drawer
       v-model="drawer"
       app
       class="pt-4"
       color="grey lighten-3"
-      mini-variant
     >
-      <v-avatar
-        v-for="n in 6"
-        :key="n"
-        :color="`grey ${n === 1 ? 'darken' : 'lighten'}-1`"
-        :size="n === 1 ? 36 : 20"
-        class="d-block text-center mx-auto mb-9"
-      ></v-avatar>
+      <v-list>
+        <v-list-item
+          v-for="([icon, text], i) in items"
+          :key="i"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <template v-slot:append>
+        <div class="pa-2">
+          
+          <v-dialog
+      v-model="dialog"
+      width="500"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="red lighten-2"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+          Click Me
+        </v-btn>
+      </template>
+
+      <v-card>
+        <v-card-title class="text-h5 grey lighten-2">
+          Privacy Policy
+        </v-card-title>
+
+        <v-card-text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            text
+            @click="dialog = false"
+          >
+            I accept
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+        </div>
+      </template>
     </v-navigation-drawer> 
 
     <v-main class="grey lighten-3">
       <v-container>
         <v-row>
-           <!-- <v-col
-            cols="12"
-            sm="2"
-          >
-            <v-sheet
-              rounded="lg"
-              min-height="268"
-            >
-              <div>
-                <b-button @click="logout">Logout</b-button>
-              </div>
-            </v-sheet>
-          </v-col>  -->
-
           <v-col
             cols="12"
             sm="8"
@@ -44,12 +84,13 @@
             <div>
                 <li v-for="server in servers" v-bind:key="server">{{ server.username }}</li>
             </div>
+
             </v-sheet>
           </v-col>
 
           <v-col
             cols="12"
-            sm="2"
+            sm="3"
           >
             <v-sheet
               rounded="lg"
@@ -81,7 +122,12 @@ export default {
     name: "AdminPage",
     data() {
         return {
-            servers:[]
+          drawer: false,
+            servers:[],
+            items: [
+        ['mdi-email', 'Inbox'],
+        ['mdi-account-supervisor-circle', 'Supervisors'],
+        ['mdi-clock-start', 'Logout'],]        
         }
     },
     created() {
