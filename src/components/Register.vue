80789b04d868609ab3189c4e760bc2b04d7c7be2
rij0 <rijalaasish0@gmail.com>
2022-05-17 17:52:29 -0500

    Updated Register component

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -10,14 +10,19 @@
               </v-card-title>
               <v-card-text>
                 <v-form @submit.prevent="register" ref="form" v-model="valid" lazy-validation>
-                  <v-text-field name="username" v-model="username" label="Email" type="text" :rules="emailRules" required></v-text-field>
-                  <v-text-field name="fullname" :rules="nameRules" v-model="fullname" label="Full Name" type="text" required></v-text-field>
-                  <v-text-field name="restaurantName" v-model="restaurantName" :rules="nameRules" label="Restaurant Name" type="text" required></v-text-field>
-                  <v-text-field id="password" name="password" v-model="password" label="Password" type="password" :rules="nameRules" required>
+                  <v-text-field name="username" v-model="username" label="Email" type="text" :rules="emailRules"
+                    required></v-text-field>
+                  <v-text-field name="fullname" :rules="nameRules" v-model="fullname" label="Full Name" type="text"
+                    required></v-text-field>
+                  <v-text-field name="restaurantName" v-model="restaurantName" :rules="nameRules"
+                    label="Restaurant Name" type="text" required></v-text-field>
+                  <v-text-field id="password" name="password" v-model="password" label="Password" type="password"
+                    :rules="nameRules" required>
                   </v-text-field>
                   <v-card-actions>
                     <v-col>
                       <v-btn type="submit" class="mb-5" block color="error">Register</v-btn>
+                      <v-btn block color="error" to="/signin">Sign In</v-btn>
                     </v-col>
                   </v-card-actions>
                 </v-form>
@@ -43,7 +48,7 @@ export default {
       nameRules: [
         v => !!v || 'Field is required',
       ],
-       emailRules: [
+      emailRules: [
         v => !!v || 'E-mail is required',
         v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
       ],
@@ -55,7 +60,7 @@ export default {
   methods: {
     async register() {
       const isValid = this.$refs.form.validate();
-      if(!isValid){
+      if (!isValid) {
         return;
       }
       const { username, password, fullname, restaurantName } = this;
@@ -78,8 +83,8 @@ export default {
       console.log(data);
       if (data.status == 200) {
         this.$router.push("/signin");
-      }else{
-        alert("Could not authenticate")
+      } else {
+        alert("Error: User with that email already exists")
       }
     }
   }
