db3e6e25fccaaba21705b3b16f8e3acf6040c376
rij0 <rijalaasish0@gmail.com>
2022-05-18 17:15:23 -0500

    Added ratings

--- src/components/Feedbacks.vue
+++ src/components/Feedbacks.vue
@@ -4,7 +4,44 @@
             <h2 class="pt-8">Feedbacks</h2>
 
         </center>
-        <h5 class="pa-5">Average Rating: {{ rating }}</h5>
+        <div class="pa-5">
+            <p>Average Rating: <b>{{ rating }}</b><v-btn small class="ml-10" @click="dialog = true">View all Ratings</v-btn>
+            </p>
+
+        </div>
+        <v-dialog v-model="dialog" width="500">
+            <v-card>
+                <v-card-title class="text-h5 grey lighten-2">
+                    All Ratings
+                </v-card-title>
+
+                <v-card-text class="pt-5">
+                    <v-data-table :headers="headers" :items="dialogRatings" :items-per-page="5">
+                        <template v-slot:item="row">
+                            <tr>
+                                <td>{{ row.item.date }}</td>
+                                <td>{{ row.item.rating }}</td>
+                                <td>
+                                    <v-icon small @click="deleteRating(row.item._id)">
+                                        mdi-delete
+                                    </v-icon>
+                                </td>
+                            </tr>
+
+                        </template>
+                    </v-data-table>
+                </v-card-text>
+
+                <v-divider></v-divider>
+
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="primary" text @click="dialog = false">
+                        Close
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
         <v-list two-line>
             <v-list-item-group v-model="selected">
                 <template v-for="(item, index) in feedbacks">
@@ -13,7 +50,8 @@
 
                             <v-list-item-content v-bind="attrs" v-on="on">
                                 <v-list-item-title v-text="item.feedback"></v-list-item-title>
-                                <v-list-item-subtitle class="text--primary" v-text="item.date.split(' ').slice(0,5).join(' ')">
+                                <v-list-item-subtitle class="text--primary"
+                                    v-text="item.date.split(' ').slice(0, 5).join(' ')">
                                 </v-list-item-subtitle>
                             </v-list-item-content>
 
@@ -46,14 +84,52 @@ export default {
     data() {
         return {
             feedbacks: [],
+            dialogRatings: [],
             rating: 0,
+            dialog: false,
+            headers: [
+                {
+                    text: 'Date',
+                    value: 'date',
+                },
+                {
+                    text: 'Rating',
+                    value: 'rating'
+                },
+                {
+                    text: 'Actions', value: 'actions', sortable: false
+                }
+            ]
         };
-    }, created() {
+    }, async created() {
         this.getFeedbacks();
         this.calculateRating();
     },
     methods: {
 
+        async deleteRating(id) {
+            const res = await fetch(
+                "http://localhost:5000/admin/ratings",
+                {
+                    method: "DELETE",
+                    credentials: 'include',
+
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        id
+                    })
+                }
+            ,);
+            const data = await res.json();
+            if (data.status === 200) {
+                await this.calculateRating();
+            } else {
+                alert("Could not delete the rating!")
+            }
+        }
+        ,
         async getFeedbacks() {
             const res = await fetch(
                 "http://localhost:5000/admin/feedbacks",
@@ -82,13 +158,14 @@ export default {
                 }
             ,);
             const data = await res.json();
-            if(data.success.length === 0){
+            if (data.success.length === 0) {
                 return;
             }
             let sum = 0;
             data.success.forEach(element => {
                 sum += element.rating;
             });
+            this.dialogRatings = data.success;
             this.rating = (sum / data.success.length).toPrecision(2);
             console.log(sum);
         },
